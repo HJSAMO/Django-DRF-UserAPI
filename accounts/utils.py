d0cf89047d626d735ea6d167bf872420ff891a21
Samo <zzangpigboy@naver.com>
2021-11-03 21:09:21 +0900

    Remove accouts.utils

--- accounts/utils.py
+++
@@ -1,8 +0,0 @@
-import re
-
-from django.core.exceptions import ValidationError
-
-
-def validate_email(email):
-    if re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email) is None:
-        raise ValidationError('Invalid email.')
